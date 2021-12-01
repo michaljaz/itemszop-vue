6f9f46c8c9793055623508e313312850e6fce195
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-12-01 12:39:13 +0100

    voucher prototype

--- src/components/Voucher.vue
+++ src/components/Voucher.vue
@@ -1,3 +1,24 @@
 <template>
-	<h1>Voucher</h1>
+	<div>
+		<h1>Voucher</h1>
+		<div class="content">
+			Vouchery można zdobywać w eventach lub przez aktywność na serwerze.
+		</div>
+		<input class="input" type="text" placeholder="Twój nick z serwera">
+		<input class="input mt-3" type="text" placeholder="Kod vouchera">
+		<button class="button is-success mt-3" @click='showError = true'>Sprawdź</button>
+		<div class="notification is-danger mt-3" v-show='showError'>
+			<button class="delete" @click='showError = false'></button>
+			Podałeś nieprawidłowy voucher!
+		</div>
+	</div>
 </template>
+<script>
+export default {
+	data () {
+		return {
+			showError: false
+		}
+	}
+}
+</script>
