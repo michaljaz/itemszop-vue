b39ec041dcd42c71b68df531c86b088d28e80029
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-12-01 16:15:25 +0100

    ładniejszy panel

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
