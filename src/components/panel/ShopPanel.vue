4ef0c6d5f52cdba3a414b662e9fd6cd64401fb35
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-12-07 21:55:53 +0100

    tabs in panel

--- src/components/panel/ShopPanel.vue
+++ src/components/panel/ShopPanel.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>To jest panel sklepu</div>
+  <div>
+    <div class="tabs">
+      <ul>
+        <li class="is-active"><a>Kokpit</a></li>
+        <li><a>Serwery</a></li>
+        <li><a>Usługi</a></li>
+        <li><a>Płatności</a></li>
+        <li><a>Vouchery</a></li>
+        <li><a>Strony</a></li>
+        <li><a>RCON</a></li>
+        <li><a>Ustawienia</a></li>
+        <li><a>Podgląd</a></li>
+      </ul>
+    </div>
+  </div>
 </template>
