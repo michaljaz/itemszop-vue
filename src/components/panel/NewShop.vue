271c487b1cd94e920b659d268384b9024bd0a10a
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-12-13 10:20:50 +0100

    dynamic shop id input

--- src/components/panel/NewShop.vue
+++ src/components/panel/NewShop.vue
@@ -1,5 +1,26 @@
 <template>
 	<div>
-		Nowy sklep
+		<h1 class="title">Nowy sklep</h1>
+		<div class="field">
+			<label class="label">Nazwa sklepu</label>
+			<div class="control">
+				<input class="input" type="text" placeholder="np. server.eu">
+			</div>
+		</div>
+		<div class="field">
+			<label class="label">Id sklepu (Będzie występować w linku: https://itemszop.js.org#/shop/{{ shopid }})</label>
+			<div class="control">
+				<input class="input" type="text" placeholder="Minimum 4 znaki" v-model="shopid">
+			</div>
+		</div>
 	</div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      shopid:""
+    }
+  }
+}
+</script>
