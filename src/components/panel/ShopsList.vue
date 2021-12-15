b493030dcfc14f07a461ad3bc6377333b2ad5d21
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-12-15 09:53:01 +0100

    new shop button

--- src/components/panel/ShopsList.vue
+++ src/components/panel/ShopsList.vue
@@ -4,9 +4,15 @@
       v-for="shop in parent.shops"
       :key="shop"
       :to="{ name: 'shop_main', params: { shopid: shop } }"
-      class="button is-medium is-light is-link is-fullwidth"
+      class="button is-medium is-light is-link is-fullwidth mt-2"
     >
       {{ shop }}
+    </router-link>
+    <router-link
+      class="button is-medium is-light is-success is-fullwidth mt-2"
+      :to="{ name: 'new_shop' }"
+    >
+      Utwórz nowy sklep
     </router-link>
   </div>
 </template>
